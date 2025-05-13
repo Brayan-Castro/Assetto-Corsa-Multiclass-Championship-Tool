d2fed03ed0e721f06a7c75945407b617af110db8
Brayan-Castro <bcastro1325@gmail.com>
2025-05-13 14:21:06 -0300

    deleted file

--- test.py
+++
@@ -1,9 +0,0 @@
-from acudpclient.client import ACUDPClient
-
-# Replace with your server address and port if needed
-client = ACUDPClient()
-client.listen()
-
-while True:
-    event = client.get_next_event(call_subscribers=False)
-    print(event)
